bf9acb6f9cb1b6a74b138bb3160aab4eca4ec94b
Faheem <132340055+AhmAdO9@users.noreply.github.com>
2023-05-09 00:36:06 +0530

    Update web_scrapping.py

--- web_scrapping.py
+++ web_scrapping.py
@@ -1,5 +1,4 @@
 import csv
-<<<<<<< HEAD
 from bs4 import BeautifulSoup
 import requests
 response = requests.get("https://stackoverflow.com/questions")
@@ -12,30 +11,7 @@ with open("stack_over_flow1.csv", "w") as file:
   for news in new:
             news1 = news.text          
             element.writerow([news1])
-=======
 
-
-
-from bs4 import BeautifulSoup
-import requests
-import json
-response = requests.get("https://stackoverflow.com/questions")
-soup = BeautifulSoup(response.text, "html.parser")
-new = soup.find_all("a", class_="s-link")
-data = json.dumps(new)
-print(data)
-
-
-
-
-
-
-# with open("stack_over_flow.csv", "w") as file:
-#         element = csv.writer(file)
-#         for news in new:
-#              news1 = news.text
-#              element.writerow([news1])
-             
 
 
 
